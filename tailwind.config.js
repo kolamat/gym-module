0389f876bd873304e26b61669ee105c6a9a85f80
kolamat <matanmidupe004@gmail.com>
2024-02-13 16:57:43 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,11 @@ export default {
         backgroundImage: (theme) => ({
             "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)"
             "mobile-home": "url('./assets/HomePageGraphic.png')"
-        })
+        }),
+        fontFamily: {
+            dmsans: ["DM Sans", "sans-serif"],
+            montserrat: ["Montserrat", "sans-seriff"]
+        },
     },
   },
   plugins: [],
