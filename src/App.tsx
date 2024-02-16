0a8cdad9adec70f840a49a3017a3be5666a28f09
kolamat <matanmidupe004@gmail.com>
2024-02-16 21:38:05 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import { useState } from "react";
 import Navbar from "./scenes/navbar/Navbar";
 
-enum SelectedPage {
-    Home = "home",
-    Benefits = "benefits",
-    OurClasses = "ourclasses",
-    ContactUs = "contactus",
-}
-
 function App() {
 const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
 
