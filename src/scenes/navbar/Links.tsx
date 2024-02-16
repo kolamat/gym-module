6f4c9ef1849f0b60b4722499b025cca1d4c61f1b
kolamat <matanmidupe004@gmail.com>
2024-02-16 20:05:09 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/scenes/navbar/Links.tsx

--- src/scenes/navbar/Links.tsx
+++ src/scenes/navbar/Links.tsx
@@ -10,7 +10,9 @@ const Links = ({
     const lowerCasePage = page.toLowerCase().replace(/ /g, "")
   return (
  <AnchorLink
- className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}`}
+ className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+  transition duration-500 hover:text-primary-300
+ `}
  href={`#${lowerCasePage}`}
  onClick={}
  >
