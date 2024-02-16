a506e398a8431ce7409f764d7c1ebb601c3e2b2e
kolamat <matanmidupe004@gmail.com>
2024-02-16 20:43:51 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/scenes/navbar/Navbar.tsx

--- src/scenes/navbar/Navbar.tsx
+++ src/scenes/navbar/Navbar.tsx
@@ -3,7 +3,10 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 
 
-type Props = {}
+type Props = {
+    selectedPage: string;
+    setSelectedPage: (value: string) => void;
+}
 
 const Navbar = (props: Props) => {
     const flexBetween = "flex items-center justify-between";
