a9ac489c2dedd08844419d6bb1b5f21348b8f74e
kolamat <matanmidupe004@gmail.com>
2024-02-16 20:48:36 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return <div className="app bg-gray-20">
     <Navbar 
     selectedPage={selectedPage}
-                         setSelectedPage={setSelectedPage} 
+    setSelectedPage={setSelectedPage} 
     />  
   </div>;
 }
