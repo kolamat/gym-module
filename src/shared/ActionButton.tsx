fb3e80fb7c0f6b267ddfd2b69d9359e2b4d9fa1a
kolamat <matanmidupe004@gmail.com>
2024-02-17 09:21:36 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/shared/ActionButton.tsx

---
+++ src/shared/ActionButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+type Props = {}
+
+const ActionButton = (props: Props) => {
+  return (
+      <AnchorLink>
+          
+    </AnchorLink>
+  )
+}
