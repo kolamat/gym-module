b5444a88f57e3d2daf0bf31d01180bc7a9669ec9
kolamat <matanmidupe004@gmail.com>
2024-02-17 19:15:43 +0100

    Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/navbar/home/home.tsx

--- src/scenes/navbar/home/home.tsx
+++ src/scenes/navbar/home/home.tsx
@@ -1,4 +1,11 @@
 import { SelectedPage } from '../../../shared/types'
+import useMediaQuery from '../../../hooks/useMediaQuery'
+import ActionButton from '../../../shared/ActionButton'
+import HomePageText from '@/assets/HomePageText.png'
+import HomePageGraphic from '@/assets/HomePageGraphic.png'
+import SponsorRedBull from '@/assets/SponsorRedBull.png'
+import SponsorForbes from '@/assets/SponsorForbes.png'
+import SponsorFortune from '@/assets/SponsorFortune.png'
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void
