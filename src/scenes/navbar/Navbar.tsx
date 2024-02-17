ba24b197a453c2d40d23047cfefa9780fff06f91
kolamat <matanmidupe004@gmail.com>
2024-02-17 08:34:10 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/scenes/navbar/Navbar.tsx

--- src/scenes/navbar/Navbar.tsx
+++ src/scenes/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Links";
 import { SelectedPage } from "../../shared/types";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 
 type Props = {
@@ -12,6 +13,7 @@ type Props = {
 
 const Navbar = ({selectedPage, setSelectedPage}: Props) => {
     const flexBetween = "flex items-center justify-between";
+    const isAboveMediumScreens = useMediaQuery
   return (
     <nav>
         <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
