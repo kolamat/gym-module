1d647d9d017946c2a19dc6cde4116d68b2da6b08
kolamat <matanmidupe004@gmail.com>
2024-02-18 16:04:36 +0100

    Your branch is ahead of 'origin/main' by 3 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/navbar/benefits/benefits.tsx

--- src/scenes/navbar/benefits/benefits.tsx
+++ src/scenes/navbar/benefits/benefits.tsx
@@ -7,7 +7,17 @@ const Benefits: Array<Object> = [
 {
     icon: <HomeModernIcon className="h-6 w-6" />,
     title: "State of the Art Facilities",
-    description: "",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+}, 
+{
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+},
+{
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 }
 ]
 
