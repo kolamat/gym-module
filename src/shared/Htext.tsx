3d759425de174b529e04912a22bb9aa5a6f8a1ed
kolamat <matanmidupe004@gmail.com>
2024-02-18 15:28:02 +0100

    On branch main Your branch is ahead of 'origin/main' by 5 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/shared/Htext.tsx

--- src/shared/Htext.tsx
+++ src/shared/Htext.tsx
@@ -6,6 +6,6 @@ type Props = {
 
 const Htext = ({ children}: Props) => {
   return (
-    <h1 className='basis-3/5 font-montserrat text-3xl font-bold'>Htext</h1 className='basis-3/5 font-montserrat text-3xl font-bold'>
+    <h1 className='basis-3/5 font-montserrat text-3xl font-bold'>{children}</h1>
   )
 }
