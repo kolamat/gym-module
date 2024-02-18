130ae2a7f2e332fe5106229b7c4d34a1a7630261
kolamat <matanmidupe004@gmail.com>
2024-02-18 15:27:04 +0100

    On branch main Your branch is ahead of 'origin/main' by 4 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	new file:   src/shared/Htext.tsx

---
+++ src/shared/Htext.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode;
+}
+
+const Htext = ({ children}: Props) => {
+  return (
+    <h1 className='basis-3/5 font-montserrat text-3xl font-bold'>Htext</h1 className='basis-3/5 font-montserrat text-3xl font-bold'>
+  )
+}
