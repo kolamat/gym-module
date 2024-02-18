fb5729248bfde43f36b63cc4d3c60f5cd46a78a2
kolamat <matanmidupe004@gmail.com>
2024-02-18 15:17:34 +0100

    On branch main Your branch is ahead of 'origin/main' by 3 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/navbar/benefits/benefits.tsx

--- src/scenes/navbar/benefits/benefits.tsx
+++ src/scenes/navbar/benefits/benefits.tsx
@@ -1,5 +1,6 @@
 import { HomeIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
 import { SelectedPage } from "../../../shared/types";
+import { motion } from "framer-motion";
 
 
 type Props = {
@@ -8,7 +9,13 @@ type Props = {
 
 const benefits = ({setSelectedPage}: Props) => {
   return (
-    <div>benefits</div>
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+<motion.div
+onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+>
+
+</motion.div>
+    </section>
   )
 }
 
