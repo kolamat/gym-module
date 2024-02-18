97dcde639b2c2c210aa54084813023fa1824461c
kolamat <matanmidupe004@gmail.com>
2024-02-18 16:07:59 +0100

    On branch main Your branch is ahead of 'origin/main' by 4 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/shared/types.ts

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -4,3 +4,9 @@ export enum SelectedPage {
     OurClasses = "ourclasses",
     ContactUs = "contactus",
 }
+
+export interface BenefitsType {
+    icon: JSX.Element
+    title: string
+    description: string
+}
