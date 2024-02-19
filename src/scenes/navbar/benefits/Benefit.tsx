f7834292516c00b6e0544cffd67f84bf563f57fe
kolamat <matanmidupe004@gmail.com>
2024-02-19 12:39:18 +0100

    On branch main Your branch is ahead of 'origin/main' by 15 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/navbar/benefits/Benefit.tsx

--- src/scenes/navbar/benefits/Benefit.tsx
+++ src/scenes/navbar/benefits/Benefit.tsx
@@ -23,8 +23,7 @@ const Benefit = ({ icon, title, description, setSelectedPage}: Props) => {
                           className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
                           onClick={() => setSelectedPage(SelectedPage.ContactUs)}
                           href={`#${SelectedPage.ContactUs}`}
-                      >
-                          
+                      >       
                           <p>Learn More</p>
                       </AnchorLink>
     </div>
