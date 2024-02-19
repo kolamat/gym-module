541676ef6d0cce795ab4e0d702ba5eb31a301ecb
kolamat <matanmidupe004@gmail.com>
2024-02-19 16:09:29 +0100

    Your branch is ahead of 'origin/main' by 20 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/navbar/benefits/benefits.tsx

--- src/scenes/navbar/benefits/benefits.tsx
+++ src/scenes/navbar/benefits/benefits.tsx
@@ -121,7 +121,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
 
             {/* BUTTON */}
             <div className="relative mt-16">
-              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles"></div>
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-Sparkles"></div>
             </div>
           </div>
         </div>
