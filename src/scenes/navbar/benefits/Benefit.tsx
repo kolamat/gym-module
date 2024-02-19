1c021cfc7535bd9aaff4eb30083a8916df0eeb28
kolamat <matanmidupe004@gmail.com>
2024-02-19 12:16:37 +0100

    Your branch is ahead of 'origin/main' by 9 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/navbar/benefits/Benefit.tsx

--- src/scenes/navbar/benefits/Benefit.tsx
+++ src/scenes/navbar/benefits/Benefit.tsx
@@ -5,7 +5,7 @@ type Props = {}
 const Benefit = (props: Props) => {
   return (
     <div className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center'>
-        <div> </div>
+        <div className='mb-4 flex justify-center'></div>
     </div>
   )
 }
