43f572225ebd737a49498b17b15c31c8274e0f01
kolamat <matanmidupe004@gmail.com>
2024-02-19 19:35:36 +0100

    On branch main Your branch is ahead of 'origin/main' by 9 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/navbar/Navbar.tsx

--- src/scenes/navbar/Navbar.tsx
+++ src/scenes/navbar/Navbar.tsx
@@ -63,6 +63,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
               </div>
             ) : (
               <button
+                title="bar"
                 type="button"
                 className="rounded-full bg-secondary-500 p-2"
                 onClick={() => setIsMenuToggled(!isMenuToggled)}
