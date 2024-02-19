be4e9f585dee8c662aa51bdaa575546e1d7e2546
kolamat <matanmidupe004@gmail.com>
2024-02-19 13:28:48 +0100

    On branch main Your branch is ahead of 'origin/main' by 3 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/navbar/benefits/benefits.tsx

--- src/scenes/navbar/benefits/benefits.tsx
+++ src/scenes/navbar/benefits/benefits.tsx
@@ -40,14 +40,24 @@ const Benefits = ({setSelectedPage}: Props) => {
 onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
 >
     {/* HEADER */}
-<div className="md:my-5 md:w-3/5">
+<motion.div 
+className="md:my-5 md:w-3/5"
+initial="hidden"
+whileInView="visible"
+viewport={{once: true, amount: 0.5 }}
+transition={{ duration: 0.5 }} 
+variants={{
+  hidden: { opacity: 0, x: -100 },
+  visible: { opacity: 1, x: 0}
+}}
+>
     <Htext>
         MORE THAN JUST A GYM
     </Htext>
     <p className="my-5 text-sm">
         We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease, we provide true care into each and every member
     </p>
-</div>
+</motion.div>
 {/* BENEFITS */}
  <motion.div 
  className="mt-5 items-center justify-between gap-8 md:flex"
