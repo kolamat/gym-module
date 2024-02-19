87bb8f7084d7a8e87bbac215ae78fa284d36e880
kolamat <matanmidupe004@gmail.com>
2024-02-19 18:59:59 +0100

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 }
