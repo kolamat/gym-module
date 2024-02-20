960c0333ff08514b81a2bfbbb2bffd3ed0d4e1ec
kolamat <matanmidupe004@gmail.com>
2024-02-20 10:15:50 +0100

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/ourclasses/OurClasses.tsx

--- src/scenes/ourclasses/OurClasses.tsx
+++ src/scenes/ourclasses/OurClasses.tsx
@@ -5,13 +5,18 @@ import image3 from "@/assets/image3.png";
 import image4 from "@/assets/image4.png";
 import image5 from "@/assets/image5.png";
 import image6 from "@/assets/image6.png";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const OurClasses = ({ setSelectedPage }: Props) => {
-  return <section></section>;
+  return (
+    <section id="ourclasses">
+      <motion.div></motion.div>
+    </section>
+  );
 };
 
 export default OurClasses;
