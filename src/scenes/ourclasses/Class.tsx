0e9e0cca400d5b92978fb2216c0473895e00df1a
kolamat <matanmidupe004@gmail.com>
2024-02-20 11:40:47 +0100

    On branch main Your branch is ahead of 'origin/main' by 22 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/ourclasses/Class.tsx

--- src/scenes/ourclasses/Class.tsx
+++ src/scenes/ourclasses/Class.tsx
@@ -3,8 +3,16 @@ import React from "react";
 type Props = {};
 
 const Class = ({ name, description, image }: Props) => {
+  const overlayStyles = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col i
+    tems-center justify-center`;
   return (
-    <li className="relative mx-5 inline-block h-[380px] w-[450px]">Class</li>
+    <li className="relative mx-5 inline-block h-[380px] w-[450px]">
+      <div className={overlayStyles}>
+        <p>{name}</p>
+        <p>{description}</p>
+      </div>
+      <img src={image} alt={`${image}`} />
+    </li>
   );
 };
 
