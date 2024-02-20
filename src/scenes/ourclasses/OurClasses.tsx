b807a808a29d3fc9459474d78246053fbb5026ad
kolamat <matanmidupe004@gmail.com>
2024-02-20 19:23:55 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/scenes/ourclasses/OurClasses.tsx

--- src/scenes/ourclasses/OurClasses.tsx
+++ src/scenes/ourclasses/OurClasses.tsx
@@ -50,7 +50,7 @@ type Props = {
 
 const OurClasses = ({ setSelectedPage }: Props) => {
   return (
-    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+    <section id="ourclasses" className="m-auto w-full bg-primary-100 py-40">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
       >
