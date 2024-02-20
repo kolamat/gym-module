4769f9b2759e288e985d3200efd5b412184a693e
kolamat <matanmidupe004@gmail.com>
2024-02-20 10:22:21 +0100

    On branch main Your branch is ahead of 'origin/main' by 3 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/ourclasses/OurClasses.tsx

--- src/scenes/ourclasses/OurClasses.tsx
+++ src/scenes/ourclasses/OurClasses.tsx
@@ -14,7 +14,9 @@ type Props = {
 const OurClasses = ({ setSelectedPage }: Props) => {
   return (
     <section id="ourclasses" className="w-full bg-primary-100 py-40">
-      <motion.div></motion.div>
+      <motion.div
+      onViewportEnter={() => setSelectedPage(SelectedPage.)}
+      ></motion.div>
     </section>
   );
 };
