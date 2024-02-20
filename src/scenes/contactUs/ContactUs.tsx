5755f9b6f95896ea2ae1f1803cb1e941e682bc45
kolamat <matanmidupe004@gmail.com>
2024-02-20 20:14:18 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/scenes/contactUs/ContactUs.tsx

--- src/scenes/contactUs/ContactUs.tsx
+++ src/scenes/contactUs/ContactUs.tsx
@@ -28,6 +28,12 @@ const ContactUs = ({ setSelectedPage }: Props) => {
           <Htext>
             <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
           </Htext>
+          <p>
+            It was popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem
+            Ipsum.
+          </p>
         </motion.div>
       </motion.div>
     </section>
