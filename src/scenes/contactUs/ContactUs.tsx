1f89e33a2cf04d26eb71e4ee62ed7658934c6c7b
kolamat <matanmidupe004@gmail.com>
2024-02-20 20:19:19 +0100

    On branch main Your branch is ahead of 'origin/main' by 3 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/contactUs/ContactUs.tsx

--- src/scenes/contactUs/ContactUs.tsx
+++ src/scenes/contactUs/ContactUs.tsx
@@ -35,6 +35,20 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             Ipsum.
           </p>
         </motion.div>
+        {/* FORM -AND-IMAGE  */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: -100 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          ></motion.div>
+        </div>
       </motion.div>
     </section>
   );
