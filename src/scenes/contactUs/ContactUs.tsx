450ee07d07639b833a44d6d91f094145d462457b
kolamat <matanmidupe004@gmail.com>
2024-02-20 19:48:48 +0100

    On branch main Your branch is ahead of 'origin/main' by 5 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/contactUs/ContactUs.tsx

--- src/scenes/contactUs/ContactUs.tsx
+++ src/scenes/contactUs/ContactUs.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { SelectedPage } from "../../shared/types";
 import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
