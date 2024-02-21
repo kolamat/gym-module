6d370ddf3737d21492d5caa329603bf98a83d414
kolamat <matanmidupe004@gmail.com>
2024-02-21 15:16:30 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/scenes/footer/Footer.tsx

--- src/scenes/footer/Footer.tsx
+++ src/scenes/footer/Footer.tsx
@@ -28,7 +28,12 @@ const Footer = () => {
           </p>
           <p>&#169; Kol_Gym All Rights Reserved</p>
         </div>
-        <div></div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Massa orci senectus</p>
+          <p className="my-5">Et gravida id et etiam</p>
+          <p>Ullamcorper vivamus</p>
+        </div>
         <div></div>
       </div>
     </footer>
