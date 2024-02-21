5df759547011ca7513a84c0bc256cce554418495
kolamat <matanmidupe004@gmail.com>
2024-02-21 12:50:56 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/contactUs/ContactUs.tsx

--- src/scenes/contactUs/ContactUs.tsx
+++ src/scenes/contactUs/ContactUs.tsx
@@ -133,12 +133,16 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true, amount: 0.5 }}
-            transition={{ duration: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
             variants={{
               hidden: { opacity: 0, x: 100 },
               visible: { opacity: 1, x: 0 },
             }}
-          ></motion.div>
+          >
+            <div className="md:before:content-evolvetext w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1]">
+              <img src={ContactUsPageGraphic} alt="contact-us-page-graphic" />
+            </div>
+          </motion.div>
         </div>
       </motion.div>
     </section>
