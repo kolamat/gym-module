f133ae970786d028e6320ccbe386838b1464b663
kolamat <matanmidupe004@gmail.com>
2024-02-21 11:56:43 +0100

    Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/scenes/navbar/Navbar.tsx

--- src/scenes/navbar/Navbar.tsx
+++ src/scenes/navbar/Navbar.tsx
@@ -28,7 +28,21 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
             {/* <img src={Logo} alt="logo" /> */}
 
             <svg
-              className="sm:w-[185px] h-[27px] whitespace-nowrap"
+              width="50"
+              height="50"
+              viewBox="0 0 94 108"
+              fill="#E1986F"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M54.0434 101.423L55.3836 102.908H57.384H72.616H82.8456L75.9332 95.3673L37.8715 53.8453L75.5678 12.2124L82.377 4.69202H72.232H57.512H55.4954L54.1533 6.19722L25.148 38.7274V9.19202V4.69202H20.648H9H4.5V9.19202V98.408V102.908H9H20.648H25.148V98.408V69.4065L54.0434 101.423Z"
+                stroke="white"
+                stroke-width="9"
+              />
+            </svg>
+
+            <svg
+              className="sm:w-[185px] h-[27px] sm:flex hidden whitespace-nowrap"
               width="99"
               height="17"
               viewBox="0 0 185 27"
